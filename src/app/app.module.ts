61cf53f81e3bd42833d6fa13d9bb7649722ac8ae
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-06-21 17:06:30 +0300

    Assignment: List of Authors

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { CoursesComponent } from './courses.component';
 import { CourseComponent } from './course/course.component';
 
 import { CoursesService } from './courses.service';
+import { AuthorsComponent } from './authors/authors.component';
+import { AuthorsService } from './authors.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CoursesComponent,
-    CourseComponent
+    CourseComponent,
+    AuthorsComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [CoursesService],
+  providers: [CoursesService, AuthorsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
