f31c78d651ceaf0e21893344d13dc142f46679cf
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-06-22 12:20:34 +0300

    ngFor usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+interface ICourse {
+  id: number,
+  name: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  viewMode = 'somethingElse';
+  courses: ICourse[];
+
+  loadCourses() {
+    this.courses = [
+      { id: 1, name: 'course1' },
+      { id: 2, name: 'course2' },
+      { id: 3, name: 'course3' }
+    ];
+  }
+
+  trackCourse(index, course) {
+    return course ? course.id : undefined;
+  }
 }
