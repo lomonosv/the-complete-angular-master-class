b9a7a26c3b045a4b62fe2334ed3e4d4b42ccfdb9
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-06-22 12:53:40 +0300

    Added custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { InputFormatDirective } from './input-format.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormatDirective
   ],
   imports: [
     BrowserModule,
