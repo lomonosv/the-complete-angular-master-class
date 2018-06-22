7e26d766180cfda61c6ab97ac9f5c9bbe33feb61
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-06-22 13:35:46 +0300

    Clean up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ZippyComponent } from './zippy/zippy.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ZippyComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
