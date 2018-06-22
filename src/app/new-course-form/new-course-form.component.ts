169cb3e442f5936cd9cc829015fa4717a4b5104f
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-06-22 16:15:06 +0300

    Assignment 6: Course Form

---
+++ src/app/new-course-form/new-course-form.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+interface ICategory {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-new-course-form',
+  templateUrl: './new-course-form.component.html',
+  styleUrls: ['./new-course-form.component.css']
+})
+export class NewCourseFormComponent {
+  categories: ICategory[] = [
+    { id: 1, name: 'Development' },
+    { id: 2, name: 'Art' },
+    { id: 3, name: 'Languages' }
+  ];
+}
