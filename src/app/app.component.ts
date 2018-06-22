18afa73b981dd6e171b78c0eb2f94343bc7f4574
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-06-22 10:17:35 +0300

    Clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,10 @@
 import { Component } from '@angular/core';
 
-import { FavoriteChangedEventArgs } from './favorite/favorite.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string;
-  post = {
-    title: 'Title',
-    isFavorite: true
-  };
-  tweet = {
-    body: 'Here is the body of a tweet...',
-    isLiked: false,
-    likesCount: 0
-  };
 
-  onFavoriteChanged(eventArgs: FavoriteChangedEventArgs) {
-    console.log('Favorite changed: ', eventArgs);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,35 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
-import { CourseComponent } from './course/course.component';
-
-import { CoursesService } from './courses.service';
-import { AuthorsComponent } from './authors/authors.component';
-import { AuthorsService } from './authors.service';
-import { SummaryPipe } from './summary.pipe';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { CasingPipe } from './casing.pipe';
-import { PanelComponent } from './panel/panel.component';
-import { LikeComponent } from './like/like.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CoursesComponent,
-    CourseComponent,
-    AuthorsComponent,
-    SummaryPipe,
-    FavoriteComponent,
-    CasingPipe,
-    PanelComponent,
-    LikeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [CoursesService, AuthorsService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
