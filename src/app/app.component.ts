49fc99fb9d9fe2083f26a50846c310af60d4026d
Vladimir Lomonos <vladimir.lomonos@dukascopy.com>
2018-06-22 12:34:55 +0300

    ngStyle usage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
 
-import { FavoriteChangedEventArgs } from './favorite/favorite.component';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  post = {
-    title: 'Title',
-    isFavorite: true
-  };
-
-  onFavoriteChanged(favorite: FavoriteChangedEventArgs) {
-    console.log('Favorite changed: ', favorite);
-  }
+  canSave = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { FavoriteComponent } from './favorite/favorite.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    FavoriteComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
